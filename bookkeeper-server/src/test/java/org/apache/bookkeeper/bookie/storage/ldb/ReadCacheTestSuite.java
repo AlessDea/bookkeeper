89369c6cbb4bbe8b44dcb28494181beefe1b1ef3
AlessDea <ale.flanker97@gmail.com>
2023-05-29 14:54:49 +0200

    added test for bookkeeper-server/bookie/storage/ldb/ReadCache.java

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/ReadCacheTestSuite.java
@@ -0,0 +1,11 @@
+package org.apache.bookkeeper.bookie.storage.ldb;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value= Suite.class)
+@Suite.SuiteClasses(value={GetTest.class, HasEntryTest.class})
+public class ReadCacheTestSuite {
+
+}
